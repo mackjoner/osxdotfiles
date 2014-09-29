157a0a2e22a95ba23df1a62b533d4694546f12d0
liuerfire <liuerfire@gmail.com>
2014-09-29 23:39:15 +0800

    update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,4 @@ alias mpscreen='mplayer tv:// -tv driver=v4l2:width=640:height=480:device=/dev/v
 alias convmid3='find -iname "*.mp3" -execdir mid3iconv -e GBK {} \;'
 alias py2_env='source ~/py_env/py2/bin/activate'
 alias py3_env='source ~/py_env/py3/bin/activate'
+alias pip_upgrade="pip list | awk '{print \$1}' | xargs pip install -U"
