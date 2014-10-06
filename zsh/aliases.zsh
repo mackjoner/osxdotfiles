b3aeece39eaaf86b7700cbfe04524eb2f4f61ecd
liuerfire <liuerfire@gmail.com>
2014-10-06 21:55:28 +0800

    update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
 alias da='du -sch'
+alias vim='nocorrect vim'
 
 alias capturescreen='scrot -t 20 -d 5'
 alias mpscreen='mplayer tv:// -tv driver=v4l2:width=640:height=480:device=/dev/video0 -fps 15 -vf screenshot'
