f45e8ec369f424c7d3772262c92380d0c77a53e2
liuerfire <liuerfire@gmail.com>
2015-04-23 10:35:20 +0800

    update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,6 @@ alias vim='nocorrect vim'
 alias capturescreen='scrot -t 20 -d 5'
 alias mpscreen='mplayer tv:// -tv driver=v4l2:width=640:height=480:device=/dev/video0 -fps 15 -vf screenshot'
 alias convmid3='find -iname "*.mp3" -execdir mid3iconv -e GBK {} \;'
-alias py2_env='source ~/py_env/py2/bin/activate'
-alias py3_env='source ~/py_env/py3/bin/activate'
+alias default_env='source ~/py_env/default/bin/activate'
+alias douban_env='source ~/py_env/douban/bin/activate'
 alias pip_upgrade="pip list | awk '{print \$1}' | xargs pip install -U"
