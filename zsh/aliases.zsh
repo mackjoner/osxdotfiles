669315757a1754afb9e7d5ae5025f2e0b3e83ead
liuerfire <liuerfire@gmail.com>
2015-05-02 01:55:48 +0800

    update

--- zsh/aliases.zsh
+++
@@ -1,9 +0,0 @@
-alias da='du -sch'
-alias vim='nocorrect vim'
-
-alias capturescreen='scrot -t 20 -d 5'
-alias mpscreen='mplayer tv:// -tv driver=v4l2:width=640:height=480:device=/dev/video0 -fps 15 -vf screenshot'
-alias convmid3='find -iname "*.mp3" -execdir mid3iconv -e GBK {} \;'
-alias default_env='source ~/py_env/default/bin/activate'
-alias douban_env='source ~/py_env/douban/bin/activate'
-alias pip_upgrade="pip list | awk '{print \$1}' | xargs pip install -U"
--- zsh/correction.zsh
+++
@@ -1,5 +0,0 @@
-setopt correctall
-#错误矫正
-zstyle ':completion:*' completer _complete _math _approximate
-zstyle ':completion:*:match:*' original only
-zstyle ':completion:*:approximate:*' max-errors 1 numeric
